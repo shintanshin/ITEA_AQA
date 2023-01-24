e16c62d26162bacbdd09051b5d357ab4ef538390
Anna_S-V <shintanshin@gmail.com>
2023-01-24 13:58:25 +0200

    Edited Main.java, added optimizations to code.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 import java.util.Scanner;
 public class Main {
-    public static int sumOfNumbers(int a, int b) {
+    public static void sumOfNumbers(int a, int b) {
         int c = a + b;
         System.out.println("Result:\n"+c);
-        return 0;
     }
     public static void main(String[] args) {
         Scanner a = new Scanner(System.in);
