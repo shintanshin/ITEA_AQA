d64e60a7ef69ddc51a6624b230e243c6bad7ebe3
Anna_S-V <shintanshin@gmail.com>
2023-01-28 19:10:24 +0200

    Added fixes to HW2.java file after review.

--- src/HW2.java
+++ src/HW2.java
@@ -1,37 +1,40 @@
 import java.util.Scanner;
-
 public class HW2 {
     public static int sumOf(int a) {
         int x = a / 100;
         int y = a % 100 / 10;
         int z = a % 10;
         int sum = x + y + z;
+        System.out.println("Sum of three digits: "+sum);
+        System.out.println();
         return sum;
     }
     public static int sumOf(String v1, String v2) {
         String a = v1;
         String b = v2;
         int z = a.length() + b.length();
+        System.out.println("Sum of two words: "+z);
+        System.out.println();
         return z;
     }
     public static boolean sumOf(short i) {
-        boolean comp = i > 0;
-        return comp;
+        if (i>0) {
+            System.out.println("Number is greater than 0");
+            return true;
+        }
+        else {
+            System.out.println("Number is lower or equal 0");
+            return false;
+        }
+    }
+    public static short input() {
+        Scanner j = new Scanner(System.in);
+        System.out.println("Enter the number");
+        return  j.nextShort();
     }
     public static void main(String[] args) {
-        Scanner i = new Scanner(System.in);
-        System.out.println("Enter the number");
-        short num = i.nextShort();
-        int overLoud = sumOf("Ukraine", "Country");
-        int overLoud1 = sumOf(367);
-        boolean overLoud2 = sumOf(num);
-
-        System.out.println(overLoud);
-        System.out.println(overLoud1);
-        if (overLoud2) {
-            System.out.println("Number is greater than 0");
-        } else {
-            System.out.println("Number is lower or equal 0");
-        }
+        sumOf(547);
+        sumOf("Ukraine", "Country");
+        sumOf(input());
     }
 }
---
+++ src/HW3.java
@@ -0,0 +1,20 @@
+public class HW3 {
+    public static void maxLength(String s1, String s2, String s3) {
+        int a = s1.length();
+        int b = s2.length();
+        int c = s3.length();
+        if (a > b && a > c) {
+            System.out.println("result:\n" + s1);
+        } else if (b > a && b > c) {
+            System.out.println("result:\n" + s2);
+        } else if (c > a && c > b) {
+            System.out.println("result:\n" + s3);
+        } else {
+            System.out.println("result:\n" + "There are no the max-length word");
+        }
+    }
+
+    public static void main(String[] args) {
+        maxLength("Дали","Горячу","Воду");
+    }
+}
